df25fed5d3919ccf8b2c71f50f938b47ba82155f
j3lmer <jelmer339@gmail.com>
2023-01-09 13:50:07 +0100

    functional dynamic routes

--- client/main.js
+++ client/main.js
@@ -1,4 +1,27 @@
 import { Urls } from '/imports/api/UrlCollection.js';
-import '/imports/ui/App.js';
+import '/imports/ui/home/App.js';
+import '/imports/ui/redirect/App.js';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import popper from 'popper.js';
+global.Popper = popper;
 
-const insertUrl = urlLink => Urls.insert({text: urlLink});
+FlowRouter.route('/', {
+    name: 'home',
+    action() {
+        BlazeLayout.render("mainContainer");
+    }
+});
+
+FlowRouter.route('/redirect/:id', {
+    name: 'redirect',
+    action() {
+
+        const param = FlowRouter.getParam("id");
+        // const entity = Urls.find({_id: param});
+        // console.log(entity);
+
+        BlazeLayout.render("redirectContainer", {data: entity});
+    }
+});
